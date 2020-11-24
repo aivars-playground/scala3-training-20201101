2e5a5cdfeff4637b61ff60440285d3809d1626d1
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2020-11-24 19:48:36 +0200

    union type

--- src/main/scala/newtypes/UnionTypeDemo.scala
+++ src/main/scala/newtypes/UnionTypeDemo.scala
@@ -6,18 +6,18 @@ trait UtCommon {
 
 class UT1(in:Int) extends UtCommon{
   val ut1field = in
-  val common = "class1"
+  val common = "classUT1"
 }
 
 class UT2(in: String) extends UtCommon{
   val ut2field = in
-  val common = "class2"
+  val common = "classUT2"
 }
 
 class UnionTypeDemo {
-  
+
   def doSomething(ut: (UT1 | UT2) & UtCommon /*this does nothing!!!!*/ ) = {
-    //ut.common cannot access
+    //ut.common is not accessible
     ut match {
       case ut:UT1 =>
         println(s"====${ut.ut1field},${ut.common}")
@@ -26,14 +26,11 @@ class UnionTypeDemo {
     }
   }
 
-  def doSomethingonCommon(ut: UtCommon): Unit = {
-    ut.common
-  }
-
   val ut1 = UT1(1)
   val ut2 = UT2("A")
 
-  doSomething(ut1)
-  doSomething(ut2)
+  val either: UT1 | UT2 = ut1
+  doSomething(either)
   
+  doSomething(ut2)
 }
